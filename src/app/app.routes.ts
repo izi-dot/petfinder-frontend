54d25c06bf22d7a6fe0a61a2fcc57d06068889ce
izi-dot <martin.kuciak.2@gmail.com>
2025-12-13 18:42:26 +0100

    vytvorenie post-page + vytvorenie navbaru

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
-import { SignUpPage } from './components';
+import { SignUpPage, LoginPage, PostPage } from './components';
 
 export const routes: Routes = [
     {
         path: '',
         component: SignUpPage
+    },
+    {
+        path: 'login',
+        component: LoginPage
+    },
+    {
+        path: 'post',
+        component: PostPage
     },
 ];
