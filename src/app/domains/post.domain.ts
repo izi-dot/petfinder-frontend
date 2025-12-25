33ac8e65a7bedb34857f529620d601b0a52ac6b9
izi-dot <martin.kuciak.2@gmail.com>
2025-12-25 20:56:37 +0100

    vytvorenie zvlast componentu pre post + vytvorenie datovej struktury pre post

---
+++ src/app/domains/post.domain.ts
@@ -0,0 +1,10 @@
+export class Post {
+  id?: string;
+  createdBy?: string;
+  createAt?: string;
+  petName?: string;
+  location?: string; // NOve mesto
+  photos?: string[];
+  intractionsCount?: number;
+  solved?: boolean;
+}
---
+++ src/app/post-only/post-only.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-only',
+  imports: [],
+  templateUrl: './post-only.html',
+  styleUrl: './post-only.scss',
+})
+export class PostOnly {
+
+}
